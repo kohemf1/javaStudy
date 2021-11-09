a232eead580764ea99ad287954d68803cbd0396f
kohemf1 <kohemf1@gmail.com>
2021-11-09 23:49:28 +0900

    baekjoon
    for

--- java/baekjoon/src/lv3/Main.java
+++ java/baekjoon/src/lv3/Main.java
@@ -19,17 +19,33 @@ public class Main {
 		
 		
 		//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
-		int a;
-		int b;
-		Scanner sc = new Scanner(System.in);
-
+//		int a;
+//		int b;
+//		int T;
+//		Scanner sc = new Scanner(System.in);
+//
+//		T = sc.nextInt();
+//		
+//		for(int i = 0; i < T; i++) {
+//			a = sc.nextInt();
+//			b = sc.nextInt();
+//			System.out.println(a + b);
+//		}
 		
-		while(true) {
-			a = sc.nextInt();
-			b = sc.nextInt();
-			System.out.println(a+b);
+		// n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+		
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		int sum = 0;
+		
+		for(int i = 1; i < n+1; i++) {
+			sum += i;
+			
 		}
 		
+		System.out.println(sum);
 	}
 	
 }
