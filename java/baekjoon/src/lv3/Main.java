0b353a28b1dfb16994e284cc9d56b3d4fbfd1a13
kohemf1 <kohemf1@gmail.com>
2021-11-10 23:38:36 +0900

    baekjoon
    BufferedReader BufferedWriter

--- java/baekjoon/src/lv3/Main.java
+++ java/baekjoon/src/lv3/Main.java
@@ -1,10 +1,16 @@
 package lv3;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		// N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 
 		// 출력 형식에 맞춰서 출력하면 된다
@@ -34,18 +40,35 @@ public class Main {
 		
 		// n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
 		
-		int n;
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
+//		int n;
+//		Scanner sc = new Scanner(System.in);
+//		n = sc.nextInt();
+//		
+//		int sum = 0;
+//		
+//		for(int i = 1; i < n+1; i++) {
+//			sum += i;
+//			
+//		}
+//		
+//		System.out.println(sum);
+//		
+		//첫 줄에 테스트케이스의 개수 T가 주어진다. T는 최대 1,000,000이다.
+		//다음 T줄에는 각각 두 정수 A와 B가 주어진다. A와 B는 1 이상, 1,000 이하이다.
 		
-		int sum = 0;
-		
-		for(int i = 1; i < n+1; i++) {
-			sum += i;
-			
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		//String s = br.readLine();
+		//StringTokenizer st = new StringTokenizer(s);		
+		//int T = Integer.parseInt(st.nextToken());
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		for(int i=0; i<T; i++) {
+			st = new  StringTokenizer(br.readLine());
+			bw.write((Integer.parseInt(st.nextToken())
+					+Integer.parseInt(st.nextToken()))+"\n");
 		}
-		
-		System.out.println(sum);
+		bw.close();
 	}
 	
 }
