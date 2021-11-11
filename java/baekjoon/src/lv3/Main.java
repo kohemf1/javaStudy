0b6c6c7645c39022704ca2777a75b1784b28a4bd
kohemf1 <kohemf1@gmail.com>
2021-11-11 23:37:13 +0900

    baekjoon
    for

--- java/baekjoon/src/lv3/Main.java
+++ java/baekjoon/src/lv3/Main.java
@@ -72,13 +72,37 @@ public class Main {
 		
 		//자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
 		
-		int N;
+//		int N;
+//		Scanner sc = new Scanner(System.in);
+//		
+//		N = sc.nextInt();
+//		
+//		for(int i=1; i < N+1; i++) {
+//			System.out.println(i);
+//		}
+		
+		//자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+//		int N;
+//		Scanner sc = new Scanner(System.in);
+//		
+//		N = sc.nextInt();
+//		
+//		for(int i=N; i > 0; i--) {
+//			System.out.println(i);
+//		}
+			
+		//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오
+		
+		int T;
 		Scanner sc = new Scanner(System.in);
 		
-		N = sc.nextInt();
-		
-		for(int i=1; i < N+1; i++) {
-			System.out.println(i);
+		T = sc.nextInt();
+		for(int i=0; i < T; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a+b);
+			
 		}
 		
 	}
