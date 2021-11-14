d346bd671ed691a8b7b7a9d7fab4dfa1d8be887c
kohemf1 <kohemf1@gmail.com>
2021-11-15 00:28:10 +0900

    baekjoon
    while

--- java/baekjoon/src/lv4/Main.java
+++ java/baekjoon/src/lv4/Main.java
@@ -22,15 +22,43 @@ public class Main {
 //			System.out.println(a+b);
 //		}
 		
+//		int a;
+//		int b;
+//		Scanner sc = new Scanner(System.in);		
+//		while(sc.hasNextInt()) {
+//			
+//			a = sc.nextInt();
+//			b = sc.nextInt();
+//			System.out.println(a+b);
+//		}		
+//		sc.close();
+//		
+		
+		// https://www.acmicpc.net/problem/1110 
+		// 백준 1110번 문제풀이
+		
+
+		Scanner sc = new Scanner(System.in);
+		
 		int a;
-		int b;
-		Scanner sc = new Scanner(System.in);		
-		while(sc.hasNextInt()) {
+		a = sc.nextInt();
+		sc.close();
+		
+		int cnt=0;
+		int result = a;
+		
+		while(true) {
 			
-			a = sc.nextInt();
-			b = sc.nextInt();
-			System.out.println(a+b);
-		}		
+			a = ((a%10)*10)+(((a/10)+(a%10))%10);
+			cnt++;
+			
+			if(a == result) {
+				break;
+			}
+			
+		}
+		System.out.println(cnt);
+		
 		
 	}
 
