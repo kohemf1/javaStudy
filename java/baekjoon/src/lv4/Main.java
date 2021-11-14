0c26957a121afe80fc1ab0a1a8c069e4262f05c9
kohemf1 <kohemf1@gmail.com>
2021-11-14 23:51:05 +0900

    baekjoon
    while

--- java/baekjoon/src/lv4/Main.java
+++ java/baekjoon/src/lv4/Main.java
@@ -1,10 +1,37 @@
 package lv4;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//두 정수 A와 B를 입력받은 다음, 
+		//A+B를 출력하는 프로그램을 작성하시오.
+		
+//		int a;
+//		int b;
+//		Scanner sc = new Scanner(System.in);		
+//		while(true) {
+//			
+//			a = sc.nextInt();
+//			b = sc.nextInt();
+//			if(a == 0 && b==0) {
+//				break;
+//			}
+//			System.out.println(a+b);
+//		}
+		
+		int a;
+		int b;
+		Scanner sc = new Scanner(System.in);		
+		while(sc.hasNextInt()) {
+			
+			a = sc.nextInt();
+			b = sc.nextInt();
+			System.out.println(a+b);
+		}		
+		
 	}
 
 }
