d8333e42797c52f7b1b51ff311099772eef98907
kohemf1 <kohemf1@gmail.com>
2021-11-17 00:10:06 +0900

    beakjoon
    max value

--- java/baekjoon/src/lv5/Main.java
+++ java/baekjoon/src/lv5/Main.java
@@ -7,21 +7,51 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//N개의 정수가 주어진다. 
-		//이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+//		//N개의 정수가 주어진다. 
+//		//이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+//		
+//		int N;
+//		Scanner sc = new Scanner(System.in);
+//		N = sc.nextInt();
+//		
+//		int[] arr = new int[N];
+//		
+//		for(int i = 0; i < N; i++) {
+//			arr[i] = sc.nextInt();
+//		}
+//		sc.close();
+//		Arrays.sort(arr);
+//		System.out.println(arr[0]+" "+arr[N-1]);
 		
-		int N;
+		// 9개의 자연수가 주어질때 최댓값을 찾고 최댓값이 몇번째 수인지 구하여라
+		
+		//int n;
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
+		//n = sc.nextInt();
 		
-		int[] arr = new int[N];
+		int n = 9;
 		
-		for(int i = 0; i < N; i++) {
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
 		
-		Arrays.sort(arr);
-		System.out.println(arr[0]+" "+arr[N-1]);
+		
+		int count = 0;
+		int max = 0;
+		int index = 0;
+		
+		for(int value : arr) {
+			count++;
+			
+			if(value > max) {
+				max = value;
+				index = count;
+			}
+		}
+		
+		System.out.print(max +"\n"+index);		
 		
 	}
 
