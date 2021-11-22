af41eaa600d6d6d3b814d9676211d71e3c51dab9
kohemf1 <kohemf1@gmail.com>
2021-11-22 23:31:51 +0900

    baekjoon
    average

--- java/baekjoon/src/lv5/Main.java
+++ java/baekjoon/src/lv5/Main.java
@@ -82,15 +82,41 @@ public class Main {
 //		//수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 
 //		//그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
 		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		HashSet<Integer> remain = new HashSet<>();
+//		
+//		for(int i = 0; i < 10; i++) {
+//			remain.add(sc.nextInt()%42);
+//		}
+//		sc.close();
+//		System.out.println(remain.size());
+//		
+		
+		//세준이는 기말고사를 망쳤다. 세준이는 점수를 조작해서 집에 가져가기로 했다. 
+		//일단 세준이는 자기 점수 중에 최댓값을 골랐다. 이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
+
+		//예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 
+		//수학점수는 50/70*100이 되어 71.43점이 된다.
+
+		//세준이의 성적을 위의 방법대로 새로 계산했을 때, 
+		//새로운 평균을 구하는 프로그램을 작성하시오.
+		
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		
-		HashSet<Integer> remain = new HashSet<>();
-		
-		for(int i = 0; i < 10; i++) {
-			remain.add(sc.nextInt()%42);
+		double[] arr = new double[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextDouble();
 		}
 		sc.close();
-		System.out.println(remain.size());
+		
+		double sum=0;
+		Arrays.sort(arr);
+		for(int i = 0; i < n; i++) {
+			sum += ((arr[i]/arr[n-1])*100);
+		}
+		System.out.println(sum/n);
 		
 		
 		
