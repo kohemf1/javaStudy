70cb5764ddf1e412c6270bc818d261664305f976
kohemf1 <kohemf1@gmail.com>
2021-12-01 23:49:56 +0900

    baekjoon
    lv6-2

--- java/baekjoon/src/lv6/Main.java
+++ java/baekjoon/src/lv6/Main.java
@@ -8,18 +8,29 @@ public class Main {
 		
 		//정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오.
 		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int n = sc.nextInt();
+//		int[] arr = new int[n];
+//		int sum = 0; 
+//		
+//		for(int i = 0; i < arr.length+1; i++) {
+//			
+//			sum += i;
+//			
+//		}
+//		System.out.println(sum);
+		
+		//예를 들어, 33으로 시작한다면 다음 수는 33 + 3 + 3 = 39이고, 
+		//그 다음 수는 39 + 3 + 9 = 51, 다음 수는 51 + 5 + 1 = 57이다. 
+		//이런식으로 다음과 같은 수열을 만들 수 있다.
+		
 		Scanner sc = new Scanner(System.in);
 		
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		int sum = 0; 
+		int a = sc.nextInt();
 		
-		for(int i = 0; i < arr.length+1; i++) {
-			
-			sum += i;
-			
-		}
-		System.out.println(sum);
+		
+		
 	}
 
 }
