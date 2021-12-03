5fe43734a383dad1c54fba11bd23d22016049d12
kohemf1 <kohemf1@gmail.com>
2021-12-03 23:00:17 +0900

    baekjoon
    1

--- java/baekjoon/src/lv7/Main.java
+++ java/baekjoon/src/lv7/Main.java
@@ -19,20 +19,28 @@ public class Main {
 //		System.out.println(b);
 //		
 		
-		//N개의 숫자가 공백 없이 쓰여있다. 
-		//이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오
+//		//N개의 숫자가 공백 없이 쓰여있다. 
+//		//이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int N = sc.nextInt();
+//		String num = sc.next();
+//		int sum = 0;
+//		
+//		for(int i = 0; i < N; i++) {
+//			sum += num.charAt(i)-'0';//아스키코드값 반환떄문에 -0해준다
+//		}
+//		
+//		System.out.println(sum);
 		
-		Scanner sc = new Scanner(System.in);
+		//각각의 알파벳에 대해서, a가 처음 등장하는 위치, b가 처음 등장하는 위치, 
+		//... z가 처음 등장하는 위치를 공백으로 구분해서 출력한다.
+
+		//만약, 어떤 알파벳이 단어에 포함되어 있지 않다면 -1을 출력한다. 
+		//단어의 첫 번째 글자는 0번째 위치이고, 두 번째 글자는 1번째 위치이다.
 		
-		int N = sc.nextInt();
-		String num = sc.next();
-		int sum = 0;
 		
-		for(int i = 0; i < N; i++) {
-			sum += num.charAt(i)-'0';//아스키코드값 반환떄문에 -0해준다
-		}
-		
-		System.out.println(sum);
 		
 	}
 
